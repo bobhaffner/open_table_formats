81e104d18cab9c66eb9cfea6c919b816d5ccbd2d
Bob Haffner <bob.haffner@gmail.com>
2024-04-09 06:44:13 -0500

    post talk push

---
+++ README.md
@@ -0,0 +1,5 @@
+### AWS Meetup (4/8/2024) Material
+
+[Slides](aws_meetup/slides/slides.pdf)  
+[Athena SQL](sql/iceberg.sql)  
+[Spark Notebook](notebooks/iceberg.ipynb)  
--- sql/iceberg.sql
+++ sql/iceberg.sql
@@ -1,4 +1,6 @@
 -- DROP TABLE sampledb.customer_orders_iceberg;
+
+
 -- create iceberg table from athena table via CTAS query
 CREATE TABLE sampledb.customer_orders_iceberg WITH (
     table_type = 'ICEBERG',
@@ -8,49 +10,63 @@ CREATE TABLE sampledb.customer_orders_iceberg WITH (
 ) AS
 SELECT *
 FROM sampledb.customer_orders;
+
+
 -- basic queries
 SELECT COUNT(*) as cnt
 FROM customer_orders_iceberg;
+
 SELECT *
 FROM customer_orders_iceberg
 LIMIT 5;
+
 SELECT orderstatus,
     COUNT(*) as cnt
 FROM customer_orders_iceberg
 GROUP BY orderstatus;
+
 -- update a phone number
 SELECT phone,
     *
 FROM customer_orders_iceberg
 WHERE custkey = '448095821'
+
 UPDATE customer_orders_iceberg
 SET phone = '8675309'
 WHERE custkey = '448095821';
+
 SELECT phone,
     *
 FROM customer_orders_iceberg
 WHERE custkey = '448095821';
+
 -- point in time query with snapshot id
 SELECT committed_at,
     snapshot_id,
     manifest_list
 FROM "customer_orders_iceberg$snapshots";
+
 SELECT phone
 FROM customer_orders_iceberg FOR VERSION AS OF 8761371467292690667
 WHERE custkey = '448095821';
+
 -- point in time query using timestamp
 SELECT phone
 FROM customer_orders_iceberg FOR TIMESTAMP AS OF (current_timestamp - interval '2' minute)
 WHERE custkey = '448095821';
+
 -- schema evolution - adding Day Of Week
 ALTER TABLE customer_orders_iceberg
 ADD COLUMNS (dow int);
+
 UPDATE customer_orders_iceberg
 SET dow = day_of_week(CAST(orderdate as date));
+
 SELECT orderdate,
     dow
 FROM customer_orders_iceberg
 LIMIT 5;
+
 SELECT committed_at,
     snapshot_id,
     manifest_list
